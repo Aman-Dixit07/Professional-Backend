ce3e9b8a66ee47a48c8a56f1b244f82034c6a2ec
Aman Dixit <amandixitbsp@gmail.com>
2025-07-24 22:11:08 +0530

    Created the error and responce class in utils

---
+++ src/utils/ApiError.js
@@ -0,0 +1,25 @@
+//we are creating a error class then we can use throughout the project any where
+
+class ApiError extends Error {
+  constructor(
+    statusCode,
+    message = "Something went wrong",
+    errors = [],
+    stack = ""
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.data = null;
+    this.message = message;
+    this.success = false;
+    this.errors = errors;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+export default ApiError;
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,26 @@
+//we will do it in 2 ways - promise and using try , catch
+
+//promise method
+
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler()).reject((err) => {
+      next(err);
+    }); //we can use catch also in place of reject
+  };
+};
+
+export { asyncHandler };
+
+//try and catch method
+
+// const asyncHandler = (requestHandler) => async (req, res, next) => {
+//   try {
+//     await requestHandler(req, res, next);
+//   } catch (error) {
+//     res.status(err.code || 500).json({
+//       message: err.message,
+//       success: false,
+//     });
+//   }
+// };
